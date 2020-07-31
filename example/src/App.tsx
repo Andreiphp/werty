b6ba90564056a8955b732cdb28e36c0b3f5839b9
Rozhko Kirill <kirill.rozhko@mrsoft.by>
2020-07-31 16:00:17 +0300

    Test_Apollo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { ExampleComponent } from 'classif-expirement'
 import 'classif-expirement/dist/index.css'
 
+
+const client: any = new ApolloClient({
+  uri: '/'
+});
+
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <ApolloProvider client={client}>
+      <ExampleComponent text={client.uri} />
+    </ApolloProvider>
+    )
 }
 
 export default App
